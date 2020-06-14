f5393e99f3db4d5978bb9d3843b5ebfd781d9881
huzaifa1287 <57413878+huzaifa1287@users.noreply.github.com>
2020-06-14 16:36:16 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,119 @@
+//Chapter1
+
+//1
+
+alert("Welcome");
+
+
+//2
+
+
+alert("Error!"+" "+"Please enter a valid password");
+
+//3
+alert("Welcome to JS Land.."+"<br>"+"Happy Coding!");
+//4
+alert("Welcome to JS Land...");
+alert("Happy Coding!");
+//5
+alert("Hello..."+""+"I can run JS through my web browser's console);
+
+
+//Chapvarter2
+
+
+
+
+var userName="Ali";
+var myName="Faizan Ahmed";
+alert(userName);
+alert(myName);
+
+
+//3
+
+
+var a="message";
+alert(a);
+
+
+
+var b="Hello World";
+
+alert(b);
+
+
+
+//4
+
+var name="Ali";
+var age="18";
+var profession="Certified Mobile Development";
+
+alert(name);
+alert(age);
+alert(profession);
+
+
+
+//5
+var i,j;
+for(j=5;i>=1;i--){
+	for(j=1;j<=i;i++){
+		document.write("PIZZA");
+	}
+	document.write("<br>");
+}
+
+
+//6
+
+var email="example@example.com";
+alert("My email address is"+" "+email);
+
+//7
+
+var book="A smarter way to learn javascript";
+alert("I am trying to learn"+book);
+
+
+//8
+
+document.write("Yah!"+" "+"I can write HTML content through javascript");
+
+
+
+
+//9
+
+alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+
+//chapter 3
+
+
+var age=20;
+alert(age);
+
+
+var n=prompt("Enter Here");
+alert("You have visited"+n +"times");
+
+
+
+var birthyear=1999;
+document.write("My birth year is "+" "+birthyear);
+
+
+var name="John Doe";
+var product=" shirts";
+var quantity=5;
+document.write(+name +"ordered" +quantity +"" +product +"on XYZ Clothing store");
+
+
+//chapter4
+
+document.write("Variable names only contain,numbers,$ and_.For example : $my_1stVariable");
+document.write("Varaible must begin with a letter, $ or_. For example:$name,_name or name");
+document.write("Variable names are case sensitive");
+document.write("Variable names should not be JS Keywords");
+
